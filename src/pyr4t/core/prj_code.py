247abcbd27954475ab2eaa031454395b51c3489e
R4tL <simon33910.lambert@gmail.com>
2025-12-28 17:22:00 +0100

    Correction command dstr

--- src/pyr4t/core/prj_code.py
+++ src/pyr4t/core/prj_code.py
@@ -56,19 +56,6 @@ class ProjectCodeM4nager:
                 cwd=str(self.proj_path),
             )
 
-    def test(self, specific: str = ""):
-        """
-        Run tests in `tests` dir.
-        Args:
-            specific (str): specific test to run (dir, file, file::fuction)
-        """
-
-        print(f"[info] Run {Path(self.proj_path).name} tests ...")
-        subprocess.check_call(
-            [sys.executable, "-m", "pytest", specific],
-            cwd=str(self.proj_path / "tests"),
-        )
-
     def run(self, script: str = "main", dev_mode: bool = False):
         """
         Run a script in the `script` dir.
@@ -160,37 +147,33 @@ class ProjectCodeM4nager:
 
         # pylint: disable=C0103
         BECON = "# TOD" + "O:"  # Dont interpret as T0D0
-        SINGLE_LINE_DOC = f"{BECON} add description"
+        FIRST_LINE_DOC = f"{BECON} update docstring"
         GOOGLE_DOC_TEMPLATE = """
 {prefix}{args}{returns}
 """
-        DO_RETURN_TYPE = f"    {BECON} add return type"
-        DO_UPDATE = f"    {BECON} update docstring"
+        DO_RETURN_TYPE = f"{BECON} add return type to the fct (-> type)"
 
         def generate_google_docstring(
-            node: ast.FunctionDef, indent: str, update=False
+            node: ast.FunctionDef, indent: str
         ) -> str:
             """
             Generate a Google-style docstring with proper indentation.
             Args:
                 node (ast.FunctionDef): the function node
                 indent (str): indentation
-                update (bool): if needs update
             Returns:
                 str: new docstring
             """
 
-            prefix = f"{DO_UPDATE}" if update else f"{SINGLE_LINE_DOC}"
-            fbd_args = ["self", "cls"]
-            filtered_args = [
-                arg.arg for arg in node.args.args if arg.arg not in fbd_args
-            ]
+            prefix = f"{FIRST_LINE_DOC}"
+            filtered_args = filter_args(node)
+
             if (
                 node.returns is None
                 and not has_non_none_return(node)
                 and not filtered_args
             ):
-                return f'{indent}"""{prefix}"""\n'
+                return f'{indent}"""{prefix}"""'
             inner_indent = indent + "    "
             args_lines = "\n".join(
                 f"{inner_indent}{arg}:" for arg in filtered_args
@@ -212,12 +195,11 @@ class ProjectCodeM4nager:
                     continue
                 if not line.startswith(inner_indent) and line not in (
                     DO_RETURN_TYPE,
-                    DO_UPDATE,
                 ):
                     docstring_body = docstring_body.replace(
                         line, indent + line
                     )
-            return f'{indent}"""{docstring_body}{indent}"""\n'
+            return f'{indent}"""{docstring_body}{indent}"""'
 
         def check_docstring_needs_update(
             node: ast.FunctionDef, docstring: str
@@ -234,28 +216,28 @@ class ProjectCodeM4nager:
 
             if docstring is None:
                 return True
-            filtered_args = [
-                arg.arg
-                for arg in node.args.args
-                if arg.arg not in ("self", "cls")
-            ]
+            filtered_args = filter_args(node)
             if (
                 node.returns is None
                 and not has_non_none_return(node)
                 and not filtered_args
-            ):
+                ):
                 return False
             for arg in filtered_args:
                 if arg not in docstring:
                     return True
-            if (
-                node.returns is not None
-                and has_non_none_return(node)
-                and "Returns" not in docstring
-                and ast.unparse(node.returns) not in docstring
-                and ast.unparse(node.returns) != "None"
-            ):
-                return True
+
+            if has_non_none_return(node):
+                if "Returns" not in docstring:
+                    return True
+                if node.returns is None and DO_RETURN_TYPE not in docstring:
+                    return True
+                if (
+                    node.returns is not None
+                    and (ast.unparse(node.returns)
+                    not in docstring.split("Returns:")[1])
+                ):
+                    return True
             return False
 
         def get_indent(line: str) -> str:
@@ -272,6 +254,29 @@ class ProjectCodeM4nager:
                 base = "    "
             return line[: len(line) - len(line.lstrip())] + base
 
+        def find_docstring_bounds(
+                lines: list[str], start: int
+        ) -> tuple[int, int] | None:
+            """
+            Find the start and end lines of a docstring.
+            Args:
+                lines (list[str]): lines
+                start (int): line to start searching from
+            Returns:
+                tuple[int, int] | None: start and end lines or None
+            """
+
+            if start >= len(lines):
+                return None
+            if '"""' not in lines[start]:
+                return None
+
+            end = start + 1
+            while end < len(lines) and '"""' not in lines[end]:
+                end += 1
+
+            return start, end + 1
+
         def process_file(path: Path):
             """
             Manage docstrings in a file.
@@ -287,7 +292,7 @@ class ProjectCodeM4nager:
             tree = ast.parse(code)
             edits = []
             if ast.get_docstring(tree) is None:
-                edits.append((0, f'"""{SINGLE_LINE_DOC}"""\n'))
+                edits.append((0, f'"""{FIRST_LINE_DOC}"""'))
             for node in ast.walk(tree):
                 if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
                     if node.name.startswith("_"):
@@ -296,20 +301,25 @@ class ProjectCodeM4nager:
                     line_idx = find_signature_end(lines, node.lineno - 1)
                     indent = get_indent(lines[line_idx])
                     if check_docstring_needs_update(node, docstring):
-                        edits.append(
-                            (
-                                line_idx + 1,
-                                generate_google_docstring(
-                                    node, indent, update=bool(docstring)
-                                ),
-                            )
+                        new_doc = generate_google_docstring(
+                            node, indent
                         )
+                        insert_at = line_idx + 1
+                        if docstring is not None:
+                            bounds = find_docstring_bounds(lines, insert_at)
+                            if bounds:
+                                start, end = bounds
+                                del lines[start:end]
+                                insert_at = start
+
+                        edits.append((insert_at, new_doc))
+
                 elif isinstance(node, ast.ClassDef):
                     docstring = ast.get_docstring(node)
                     line_idx = find_signature_end(lines, node.lineno - 1)
                     indent = get_indent(lines[line_idx])
                     if docstring is None:
-                        doc = f'{indent}"""{SINGLE_LINE_DOC}"""\n'
+                        doc = f'{indent}"""{FIRST_LINE_DOC}"""'
                         edits.append((line_idx + 1, doc))
             for lineno, doc in sorted(edits, reverse=True):
                 lines.insert(lineno, doc)
@@ -348,10 +358,50 @@ class ProjectCodeM4nager:
                     return True
             return False
 
+        def filter_args(node: ast.FunctionDef) -> list[str]:
+            """
+            Filter out unwanted arguments.
+            Args:
+                node (ast.FunctionDef): the function node
+            Returns:
+                list[str]: filtered arguments
+            """
+
+            fbd_args = ["self", "cls"]
+
+            # Map argument names to their default values
+            defaults = node.args.defaults
+            defaults_map = {}
+            for i, default in enumerate(defaults):
+                arg_index = len(node.args.args) - len(defaults) + i
+                defaults_map[node.args.args[arg_index].arg] = default
+
+            # Build argument list
+            filtered_args = []
+            for arg in node.args.args:
+                if arg.arg in fbd_args:
+                    continue
+                name = arg.arg
+                default = defaults_map.get(name)
+                if arg.annotation:
+                    ann = ast.unparse(arg.annotation)
+                    if default:
+                        ann = f"{ann}, optional"
+                    name = f"{name} ({ann})"
+                else:
+                    if default:
+                        ann = ", optional"
+                    else:
+                        ann = ""
+                    name = f"{name} ({BECON} Add type in fct{ann})"
+                filtered_args.append(name)
+            return filtered_args
+
         specific_path = self.proj_path / "src" / specific
         if not specific_path.exists():
             raise FileNotFoundError(f"Path not found: {specific_path}")
         if specific_path.is_file():
+            print(f"[info] Processing {specific_path.name}")
             process_file(specific_path)
         if specific_path.is_dir():
             for py_file in specific_path.rglob("*.py"):
@@ -384,6 +434,19 @@ class ProjectCodeM4nager:
         )
         subprocess.check_call(
             [sys.executable, "-m", "isort", str(specific_path)]
+        )
+
+    def test(self, specific: str = ""):
+        """
+        Run tests in `tests` dir.
+        Args:
+            specific (str): specific test to run (dir, file, file::fuction)
+        """
+
+        print(f"[info] Run {Path(self.proj_path).name} tests ...")
+        subprocess.check_call(
+            [sys.executable, "-m", "pytest", specific],
+            cwd=str(self.proj_path / "tests"),
         )
 
     def venv(self):
