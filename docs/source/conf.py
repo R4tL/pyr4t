d64d6b48b51d413e188ce20dec59a958ad5ef071
R4tL <simon33910.lambert@gmail.com>
2026-07-08 12:28:04 +0200

    Clean up documentation configuration
    Remove boilerplate comments from Sphinx configuration files (conf.py and index.rst). Update sphinx-apidoc to document the src directory directly instead of src/pyr4t, and update the toctree reference from 'pyr4t' to 'modules' to match the generated documentation structure.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
 
@@ -16,9 +8,6 @@ copyright = '2026, R4tL'
 author = 'R4tL'
 release = '1.1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
@@ -28,11 +17,6 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,8 +1,3 @@
-.. Pyr4t documentation master file, created by
-   sphinx-quickstart on Wed Jul  8 10:11:39 2026.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Pyr4t
 ===================
 
@@ -12,4 +7,4 @@ Welcome to the Pyr4t documentation!
    :maxdepth: 2
    :caption: Contents:
 
-   pyr4t
+   modules
